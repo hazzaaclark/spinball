c6f89cf9bc3bcf6f9443fa15f0799a6d00d4457f
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-29 00:27:45 +0100

    added set cartridge macro

--- src/game/cartridge.asm
+++ src/game/cartridge.asm
@@ -25,6 +25,24 @@ COUNTRY_CODE_JP:    DC.W $2A3F, DC.B 'J        '
 
                     ENDM
 
+;; SET THE CARTRIDGE ENTRY POINT BY DEFINING A CONSTANT LONG
+;; THAT WHEN INITIALISED, IT WILL CORRESPOND WITH THE RAM
+;; READ AND WRITE
+
+.SECTION: SET_CARTRIDGE
+
+SET_CARTRIDGE:      MACRO
+
+CART_START:         DC.L 0
+
+CART_ENTRY_POINT:   DC.L $FFFFF
+                    DC.L $FF0000
+                    DC.L $FFFFF
+
+CART_ENTRY_INIT:    DC.L $20202020
+ 
+                     ENDM
+
 ;----------------------------------------------------------
 ;                       END OF FILE
 ;----------------------------------------------------------
