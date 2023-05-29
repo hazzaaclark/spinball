cbc77b73d53f53185900df39da5a4559ab4b3928
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-29 01:07:58 +0100

    added vsram support

--- src/misc/macros.asm
+++ src/misc/macros.asm
@@ -13,9 +13,29 @@ ARG_VALUE:      EQU 1
 
 @LOOP\@         DC.B 1, %ARG_VALUE, 0
                 DC.B 1, %ARG_VALUE, \2
-		BNE.S @LOOP\@
+				BNE.S @LOOP\@
 
-		ENDM 
+				ENDM 
+
+
+ENABLE_VSRAM:         EQU           0
+VSRAM_ADDR:           EQU           3
+
+SUPPORT_VSRAM:      MACRO
+
+@LOOP\@             if ENABLE_VSRAM = 1
+                    DC.B $2A3F + VSRAM_ADDR << 3, $20 ;; ASSUME THE LENGTH OF THE
+					                                  ;; VSRAM CONSTANT, ACCESS THE ADDRESS IN PROPORTION TO THAT
+
+                    else 
+					
+					DC.L $20202020 				      ;; ENABLE THE VSRAM ACCORDINGLY
+					DC.L $20202020					  ;; AND DEFINE WHICH ADDRESS IT SHOULD
+                    DC.B "       "                    ;; EXECUTE IT'S RUNTIME AT
+					BNE.S @LOOP\@
+
+					ENDM
+
 
 ;----------------------------------------------------------
 ;                       END OF FILE
